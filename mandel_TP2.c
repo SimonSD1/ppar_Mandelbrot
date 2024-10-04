6f07cf4985a53ef62933b4ba309a0454338a17f6
Simon <simonsepiolduchemin@gmail.com>
2024-10-04 21:50:44 +0200

    format

--- mandel_TP2.c
+++ mandel_TP2.c
@@ -4,9 +4,10 @@
  */
 #include <stdlib.h>
 #include <stdio.h>
-#include <math.h>               /* compile with -lm */
+#include <math.h> /* compile with -lm */
 #include <sys/time.h>
-#include <arpa/inet.h>          /* htonl */
+#include <arpa/inet.h> /* htonl */
+#include <mpi.h>
 
 #include "rasterfile.h"
 
@@ -27,18 +28,18 @@ Some examples of execution:\n\
 
 double wallclock_time()
 {
-	struct timeval tmp_time;
-	gettimeofday(&tmp_time, NULL);
-	return tmp_time.tv_sec + (tmp_time.tv_usec * 1.0e-6);
+    struct timeval tmp_time;
+    gettimeofday(&tmp_time, NULL);
+    return tmp_time.tv_sec + (tmp_time.tv_usec * 1.0e-6);
 }
 
 unsigned char cos_component(int i, double freq)
 {
-	double iD = i;
-	iD = cos(iD / 255.0 * 2 * M_PI * freq);
-	iD += 1;
-	iD *= 128;
-	return iD;
+    double iD = i;
+    iD = cos(iD / 255.0 * 2 * M_PI * freq);
+    iD += 1;
+    iD *= 128;
+    return iD;
 }
 
 /**
@@ -46,43 +47,47 @@ unsigned char cos_component(int i, double freq)
  */
 void save_rasterfile(char *name, int largeur, int hauteur, unsigned char *p)
 {
-	FILE *fd = fopen(name, "w");
-	if (fd == NULL) {
-		perror("Error while opening output file. ");
-		exit(1);
-	}
+    FILE *fd = fopen(name, "w");
+    if (fd == NULL)
+    {
+        perror("Error while opening output file. ");
+        exit(1);
+    }
 
-	struct rasterfile file;
-	file.ras_magic = htonl(RAS_MAGIC);
-	file.ras_width = htonl(largeur);	            /* width of the image, in pixels */
-	file.ras_height = htonl(hauteur);	            /* height of the image, in pixels */
-	file.ras_depth = htonl(8);	                    /* depth of each pixel (1, 8 or 24 ) */
-	file.ras_length = htonl(largeur * hauteur);	    /* size of the image in nb of bytes */
-	file.ras_type = htonl(RT_STANDARD);	            /* file type */
-	file.ras_maptype = htonl(RMT_EQUAL_RGB);
-	file.ras_maplength = htonl(256 * 3);
-	fwrite(&file, sizeof(struct rasterfile), 1, fd);
+    struct rasterfile file;
+    file.ras_magic = htonl(RAS_MAGIC);
+    file.ras_width = htonl(largeur);            /* width of the image, in pixels */
+    file.ras_height = htonl(hauteur);           /* height of the image, in pixels */
+    file.ras_depth = htonl(8);                  /* depth of each pixel (1, 8 or 24 ) */
+    file.ras_length = htonl(largeur * hauteur); /* size of the image in nb of bytes */
+    file.ras_type = htonl(RT_STANDARD);         /* file type */
+    file.ras_maptype = htonl(RMT_EQUAL_RGB);
+    file.ras_maplength = htonl(256 * 3);
+    fwrite(&file, sizeof(struct rasterfile), 1, fd);
 
-	/* Color palette: red component */
-	for (int i = 255; i >= 0; i--) {
-		unsigned char o = cos_component(i, 13.0);
-		fwrite(&o, sizeof(unsigned char), 1, fd);
-	}
+    /* Color palette: red component */
+    for (int i = 255; i >= 0; i--)
+    {
+        unsigned char o = cos_component(i, 13.0);
+        fwrite(&o, sizeof(unsigned char), 1, fd);
+    }
 
-	/* Color palette: green component */
-	for (int i = 255; i >= 0; i--) {
-		unsigned char o = cos_component(i, 5.0);
-		fwrite(&o, sizeof(unsigned char), 1, fd);
-	}
+    /* Color palette: green component */
+    for (int i = 255; i >= 0; i--)
+    {
+        unsigned char o = cos_component(i, 5.0);
+        fwrite(&o, sizeof(unsigned char), 1, fd);
+    }
 
-	/* Color palette: blue component */
-	for (int i = 255; i >= 0; i--) {
-		unsigned char o = cos_component(i + 10, 7.0);
-		fwrite(&o, sizeof(unsigned char), 1, fd);
-	}
+    /* Color palette: blue component */
+    for (int i = 255; i >= 0; i--)
+    {
+        unsigned char o = cos_component(i + 10, 7.0);
+        fwrite(&o, sizeof(unsigned char), 1, fd);
+    }
 
-	fwrite(p, largeur * hauteur, sizeof(unsigned char), fd);
-	fclose(fd);
+    fwrite(p, largeur * hauteur, sizeof(unsigned char), fd);
+    fclose(fd);
 }
 
 /**
@@ -101,23 +106,24 @@ void save_rasterfile(char *name, int largeur, int hauteur, unsigned char *p)
  */
 unsigned char xy2color(double a, double b, int depth)
 {
-	double x = 0;
-	double y = 0;
-	for (int i = 0; i < depth; i++) {
-		/* save the former value of x (which will be erased) */
-		double temp = x;
-		/* new values for x and y */
-		double x2 = x * x;
-		double y2 = y * y;
-		x = x2 - y2 + a;
-		y = 2 * temp * y + b;
-		if (x2 + y2 > 4.0)
-			return (i % 255);   /* diverges */
-	}
-	return 255;   /* did not diverge in depth steps */
+    double x = 0;
+    double y = 0;
+    for (int i = 0; i < depth; i++)
+    {
+        /* save the former value of x (which will be erased) */
+        double temp = x;
+        /* new values for x and y */
+        double x2 = x * x;
+        double y2 = y * y;
+        x = x2 - y2 + a;
+        y = 2 * temp * y + b;
+        if (x2 + y2 > 4.0)
+            return (i % 255); /* diverges */
+    }
+    return 255; /* did not diverge in depth steps */
 }
 
-/* 
+/*
  * Main: in each point of the grid, apply xy2color
  */
 int main(int argc, char **argv)
@@ -132,73 +138,189 @@ int main(int argc, char **argv)
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &p);
 
+    if (argc == 1)
+    {
+        fprintf(stderr, "%s\n", info);
+        return 1;
+    }
 
-	if (argc == 1) {
-		fprintf(stderr, "%s\n", info);
-		return 1;
-	}
+    /* Default values for the fractal */
+    double xmin = -2; /* Domain of computation, in the complex plane */
+    double ymin = -2;
+    double xmax = 2;
+    double ymax = 2;
+    int w = 3840; /* dimensions of the image (4K HTDV!) */
+    int h = 3840;
+    int depth = 10000; /* depth of iteration */
 
-	/* Default values for the fractal */
-	double xmin = -2;     /* Domain of computation, in the complex plane */
-	double ymin = -2;
-	double xmax = 2;
-	double ymax = 2;
-	int w = 3840;         /* dimensions of the image (4K HTDV!) */
-	int h = 3840;
-	int depth = 10000;     /* depth of iteration */
+    /* Retrieving parameters */
+    if (argc > 1)
+        w = atoi(argv[1]);
+    if (argc > 2)
+        h = atoi(argv[2]);
+    if (argc > 3)
+        xmin = atof(argv[3]);
+    if (argc > 4)
+        ymin = atof(argv[4]);
+    if (argc > 5)
+        xmax = atof(argv[5]);
+    if (argc > 6)
+        ymax = atof(argv[6]);
+    if (argc > 7)
+        depth = atoi(argv[7]);
 
-	/* Retrieving parameters */
-	if (argc > 1)
-		w = atoi(argv[1]);
-	if (argc > 2)
-		h = atoi(argv[2]);
-	if (argc > 3)
-		xmin = atof(argv[3]);
-	if (argc > 4)
-		ymin = atof(argv[4]);
-	if (argc > 5)
-		xmax = atof(argv[5]);
-	if (argc > 6)
-		ymax = atof(argv[6]);
-	if (argc > 7)
-		depth = atoi(argv[7]);
+    /* Computing steps for increments */
+    double xinc = (xmax - xmin) / (w - 1);
+    double yinc = (ymax - ymin) / (h - 1);
 
-	/* Computing steps for increments */
-	double xinc = (xmax - xmin) / (w - 1);
-	double yinc = (ymax - ymin) / (h - 1);
+    /* show parameters, for verification */
+    // fprintf(stderr, "Domain: [%g,%g] x [%g,%g]\n", xmin, ymin, xmax, ymax);
+    // fprintf(stderr, "Increment: %g, %g\n", xinc, yinc);
+    // fprintf(stderr, "depth: %d\n", depth);
+    // fprintf(stderr, "Image size: %d x %d\n", w, h);
 
-	/* show parameters, for verification */
-	fprintf(stderr, "Domain: [%g,%g] x [%g,%g]\n", xmin, ymin, xmax, ymax);
-	fprintf(stderr, "Increment: %g, %g\n", xinc, yinc);
-	fprintf(stderr, "depth: %d\n", depth);
-	fprintf(stderr, "Image size: %d x %d\n", w, h);
+    /* Allocate memory for the output array */
+    unsigned char *image = malloc(w * h);
+    if (image == NULL)
+    {
+        perror("Error while allocating memory for array: ");
+        exit(1);
+    }
 
-	/* Allocate memory for the output array */
-	unsigned char *image = malloc(w * h);
-	if (image == NULL) {
-		perror("Error while allocating memory for array: ");
-		exit(1);
-	}
+    int nbComputed = 0;
 
-	/* start timer */
-	double start = wallclock_time();
+    int mon_rang = my_rank;
 
-	/* Process the grid point by point */
-	double y = ymin;
-	for (int i = 0; i < h; i++) {
-		double x = xmin;
-		for (int j = 0; j < w; j++) {
-			image[j + i * w] = xy2color(x, y, depth);
-			x += xinc;
-		}
-		y += yinc;
-	}
+    int nbTiles = 20;
 
-	/* stop timer */
-	double end = wallclock_time();
-	fprintf(stderr, "Total computing time: %g sec\n", end - start);
+    int ligne_par_process = h/nbTiles;
 
-	/* Save the image in the output file "mandel.ras" */
-	save_rasterfile("mandel.ras", w, h, image);
-	return 0;
+    //  numero de travail -1
+    int noMoreWork = 0;
+
+    // la premiere case contient le numero du travail
+    unsigned char *localBuff = malloc(ligne_par_process*w * sizeof(unsigned char) + 1);
+    unsigned char *localBuffRecepetion = malloc(ligne_par_process*w * sizeof(unsigned char) + 1);
+
+    // tag utilisés
+    // demande = 0
+    // resultat = 1
+    // noMoreWork =3
+
+    int demande = 0;
+
+    int demandeur_travail;
+
+    unsigned char numeroTravail;
+
+    MPI_Request request;
+
+    while (nbComputed < nbTiles && !noMoreWork)
+    {
+        if (mon_rang != 0)
+        {
+            // demande travail a 0
+            MPI_Isend(&mon_rang, 1, MPI_INT32_T, 0, 0, MPI_COMM_WORLD, &request);
+            //printf(" %d demande un tarvail\n",mon_rang);
+            // recupere le numero du travail
+
+            MPI_Irecv(&numeroTravail, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &request);
+            MPI_Wait(&request, &status);
+
+            //printf("%d a recu le travail %d\n",mon_rang,numeroTravail);
+
+            if (status.MPI_TAG == 3)
+            {   
+                //printf("%d a recu qu'il n'y a plus de travail\n",mon_rang);
+                noMoreWork = 1;
+                continue;
+            }
+
+            // calcul
+
+            //printf("%d calcul\n",mon_rang);
+
+            for(int i=0; i<ligne_par_process*w;i++){
+
+                localBuff[i+1]=mon_rang+1;
+            }
+
+            // envoit le resultat a 0
+            localBuff[0] = numeroTravail;
+            MPI_Isend(localBuff, ligne_par_process*w+ 1, MPI_UNSIGNED_CHAR, 0, 1, MPI_COMM_WORLD, &request);
+            //printf("%d a envoye sont travail a 0\n",mon_rang);
+        }
+
+        if (mon_rang == 0)
+        {
+            // attend demande travail
+            MPI_Irecv(&demandeur_travail, 1, MPI_INT32_T, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &request);
+            MPI_Wait(&request, &status);
+            //printf("0 a recue une demande de %d\n",demandeur_travail);
+
+            // envoit travail numero nbComputed et nbComputed++ si il y en a
+            MPI_Isend(&nbComputed, 1, MPI_INT32_T, demandeur_travail, 0, MPI_COMM_WORLD, &request);
+            nbComputed++;
+            //printf("0 a envoye le travail %d a %d\n",nbComputed-1,demandeur_travail);
+            // recuperere le resultat
+
+            MPI_Irecv(localBuffRecepetion, ligne_par_process*w + 1, MPI_UNSIGNED_CHAR, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &request);
+            MPI_Wait(&request, &status);
+            printf("0 a recue le travail numero %d\n",localBuffRecepetion[0]);
+            
+            unsigned char numeroTravailRecue = localBuffRecepetion[0];
+            // printf("numero travail recue : %d\n",numeroTravailRecue);
+
+            for (int i = 0; i < w * ligne_par_process+1; i++)
+            {
+                
+                printf("%d ",localBuffRecepetion[i]);
+
+                
+                
+
+                // printf("copie en %d pour le travail numero %d\n", numeroTravailRecue * (w * h / nbTiles) + i, numeroTravailRecue);
+                image[numeroTravailRecue * ligne_par_process*w + i] = localBuffRecepetion[i + 1];
+                //printf("%d ",numeroTravailRecue * ligne_par_process*w + i);
+            }
+            printf("w*ligneparprocess : %d",w*ligne_par_process);
+            printf("\n");
+        }
+    }
+
+    if (mon_rang == 0)
+    {
+        noMoreWork = 1;
+        for (int i = 1; i < p; i++)
+        {
+            MPI_Isend(&noMoreWork, 0, MPI_INT, i, 3, MPI_COMM_WORLD, &request);
+        }
+        for (int i = 0; i < h * w; i++)
+        {
+            //printf("%d ", image[i]);
+        }
+    }
+
+    // faire un broadcast pour dire que c'est finit
+
+    /* start timer */
+    // double start = wallclock_time();
+
+    /* Process the grid point by point */
+
+    /* stop timer */
+    // double end = wallclock_time();
+    // fprintf(stderr, "Total computing time: %g sec\n", end - start);
+
+    /* Save the image in the output file "mandel.ras" */
+    if (my_rank == 0)
+    {
+        for(int i=0; i<h*w; i++){
+            printf("%d ",image[i]);
+        }
+        save_rasterfile("mandel.ras", w, h, image);
+    }
+
+    MPI_Finalize();
+    return 0;
 }
